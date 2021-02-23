7aeca45a9833f650ee0d4eba0ea396f57a34b084
shaoming <657590764@qq.com>
2021-02-23 19:12:06 +0800

    renew files

--- dataStructure/recurrent.py
+++ dataStructure/recurrent.py
@@ -3,6 +3,7 @@
 # 2.将最后一个盘子移到终点柱子；
 # 3.借助起点，将高度为height-1的一叠盘子从中间柱子移到终点柱子
 
+
 class Stack():
     def __init__(self, height=0):
         self.items = []
@@ -33,3 +34,46 @@ def moveDisk(fromPole, toPole, stacking):
 fromPole = Stack()
 toPole = Stack()
 withPole = Stack()
+
+
+def searchForm(maze, startRow, startColumn):
+    maze.updatePosition(startRow, startColumn)
+
+
+# 小乌龟探索迷宫
+from turtle import *
+
+
+class Maze(object):
+    # 读入一个代表迷宫的数据文件，初始化迷宫的内部表示，
+    # 并且找到小乌龟的其实位置
+    def __init__(self, mazeFileName):
+        rowsInMaze = 0
+        columnsInMaze = 0
+        self.mazelist = []
+        mazeFile = open(mazeFileName, 'r')
+        rowsInMaze = 0
+        for line in mazeFileName:
+            rowList = []
+            col = 0
+            for ch in line[:-1]:
+                rowList.append(ch)
+                if ch == 'S':
+                    self.startRow = rowsInMaze
+                    self.startCol = col
+                col = col + 1
+            rowsInMaze = rowsInMaze + 1
+            self.mazelist.append(rowList)
+            columnsInMaze = len(rowList)
+
+        self.rowsInMaze = rowsInMaze
+        self.columnsInMaze = columnsInMaze
+        self.xTranslate = -columnsInMaze / 2
+        self.yTranslate = rowsInMaze / 2
+        self.t = Turtle(shape='turtle')
+        self.t.getscreen().setup(width=600, height=600)
+        self.t.getscreen().setworldcoordinates(-(columnsInMaze - 1) / 2 - 0.5,
+                                               -(rowsInMaze - 1) / 2 - 0.5,
+                                               (columnsInMaze - 1) / 2 + 0.5,
+                                               (rowsInMaze - 1) / 2 + 0.5
+                                               )
